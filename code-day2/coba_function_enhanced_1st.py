9bde5115a0e29018369b0f211fd255eef6bb05c9
Riffadi Taufik Hidayah <riffadi.th@gmail.com>
2019-07-18 21:25:39 +0700

    change 'str' attribute and remove condition

--- code-day2/coba_function_enhanced_1st.py
+++ code-day2/coba_function_enhanced_1st.py
@@ -1,15 +1,14 @@
 def print_judul(text):
-    print(text.uppercase())
-    print('='*( len(text) if len(text)%5==0 else 5(len(text)//5+1)
-
+    print(text.upper())
+    print('=' * len(text))
 def print_isi(daftar_isi):
     for nomor, isi in enumerate(daftar_isi):
         print("{}. {}".format(nomor, isi.title()))
 
 # Sekarang kita akan membuat tampilan seperti script sebelumnya
-# dengan menggunakan 2 fungsi diatas
+#  dengan menggunakan 2 fungsi diatas
 print_judul('bagian satu - pengenalan')
-print_isi(['pengenalan python', 'aplikasi python di kehidupan', 'siapa saja'])
+print_isi(['pengenalan python', 'aplikasi python di kehidupan nyata', 'siapa saja'])
 print_judul('bagian dua - pembahasan dasar')
 print_isi(['variabel', 'tipe data di python'])
 print_judul('bagian tiga - perulangan')
